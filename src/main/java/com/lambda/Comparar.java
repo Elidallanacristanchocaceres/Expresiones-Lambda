c622c720ed7221506fc8f4d2f5396ad551c1f54b
ElidallanaCristancho <cristanchodayana062017@gmail.com>
2025-03-13 14:42:05 -0500

    feat: :tada: Se agrego archivo persona
    Se agrego archivo persona

--- src/main/java/com/lambda/Comparar.java
+++ src/main/java/com/lambda/Comparar.java
@@ -1,5 +1,21 @@
 package com.lambda;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 public class Comparar {
-    
+public static void main(String[] args) {
+List<persona> personas = new ArrayList<>();
+personas.add(new persona("Carlos", 25));
+personas.add(new persona("Ana", 23));
+personas.add(new persona("Jose", 30));
+personas.add(new persona("Maria", 20));
+// Usar un Comparator con expresión lambda para ordenar por edad
+Comparator<persona> porEdad = (p1, p2) -> Integer.compare(p1.getEdad(),
+p2.getEdad());
+Collections.sort(personas, porEdad);
+// Mostrar la lista ordenada
+personas.forEach(System.out::println);
+}
 }
---
+++ src/main/java/com/lambda/persona.java
@@ -0,0 +1,20 @@
+package com.lambda;
+
+public class persona {
+    private String nombre;
+    private int edad;
+    public persona(String nombre, int edad) {
+    this.nombre = nombre;
+    this.edad = edad;
+    }
+    public String getNombre() {
+    return nombre;
+    }
+    public int getEdad() {
+    return edad;
+    }
+    @Override
+    public String toString() {
+    return nombre + " - " + edad;
+    }
+    }
